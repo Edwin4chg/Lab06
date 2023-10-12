a68b44356ca076888ca2a3fbafc163f487371903
Edwin Chigne <98482516+Edwin4chg@users.noreply.github.com>
2023-10-11 19:55:26 -0500

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-    define('_HOST_NAME', 'localhost');
-    define('_DATABASE_NAME', 'week');
-    define('_DATABASE_USER_NAME', 'root');
-    define('_DATABASE_PASSWORD', '');
+    define('_HOST_NAME', 'sql12.freemysqlhosting.net');
+    define('_DATABASE_NAME', 'sql12652673');
+    define('_DATABASE_USER_NAME', 'sql12652673');
+    define('_DATABASE_PASSWORD', 'VLPwWd999H');
 
     $MySQLiconn = new MySQLi(_HOST_NAME,  _DATABASE_USER_NAME, _DATABASE_PASSWORD,  _DATABASE_NAME);
 
